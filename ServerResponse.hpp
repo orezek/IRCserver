00f0c8e461c2314ed705d4623e00719451103b46
O.R <orezek@seznam.cz>
2024-09-20 12:17:35 +0200

    Implement: ServerResponse

--- ServerResponse.hpp
+++ ServerResponse.hpp
@@ -6,11 +6,29 @@
 /*   By: orezek <orezek@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 23:09:35 by orezek            #+#    #+#             */
-/*   Updated: 2024/09/17 23:10:40 by orezek           ###   ########.fr       */
+/*   Updated: 2024/09/20 12:15:28 by orezek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
-
+#include <string>
 // To be implemented
 // {client_fd, action, data}
+class ServerResponse
+{
+	public:
+		ServerResponse(void);
+		ServerResponse(const ServerResponse &obj);
+		ServerResponse &operator=(const ServerResponse &boj);
+		~ServerResponse(void);
+		std::string &getResponse(void);
+		void setResponse(std::string &response);
+		int getClientFd(void);
+		void setClientFd(int clientFd);
+		int getAction(void);
+		void setAction(int action);
+	private:
+		int action;
+		int clientFd;
+		std::string data;
+};
