629f21937db10f3aa5ef28b7f5a4f8bf65d200e3
O.R <orezek@seznam.cz>
2024-09-23 21:42:55 +0200

    Implement: initiation of serverData rooms and users

--- ServerData.cpp
+++ ServerData.cpp
@@ -6,12 +6,12 @@
 /*   By: orezek <orezek@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/22 20:01:51 by orezek            #+#    #+#             */
-/*   Updated: 2024/09/22 23:28:03 by orezek           ###   ########.fr       */
+/*   Updated: 2024/09/23 18:41:11 by orezek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ServerData.hpp"
 
-ServerData::ServerData() : fileDsDb() {};
+ServerData::ServerData() : fileDsDb(), rooms(), users() {};
 
 ServerData::~ServerData() {};
