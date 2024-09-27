d3c7d68be6ae47a244ed6198e0fb8739d3a14620
m-bartos <m.bartos96@seznam.cz>
2024-09-27 15:59:44 +0200

    Update: reformat NickCommand class

--- NickCommand.cpp
+++ NickCommand.cpp
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 16:42:28 by mbartos           #+#    #+#             */
-/*   Updated: 2024/09/27 14:38:50 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/09/27 15:58:44 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,23 +24,15 @@ NickCommand::NickCommand(ServerData& serverData, ClientMessage& clientMessage) :
 	if (oldNick == "")
 		oldNick = "*";
 
-	// parse clientMessage.parametersString by space
-	// this->parseClientMessage();
 	newNick = clientMessage.getFirstParameter();
 
-	// check if there is enough parameters for nick change (at least 1)
-	// if not - send error message, not enough parameters - 431 ERR_NONICKNAMEGIVEN
-	// check if the nickname is valid - if not 432 ERR_ERRONEUSNICKNAME
-	// check if the nickname is not same as the previous one - if so - 433 ERR_NICKNAMEINUSE
-	// check if the nickname is not already in use - if it is - 436 ERR_NICKCOLLISION
-	// change the nickname if everything is ok
 	if (newNick.empty() || newNick == "")
 	{
+		// add servername prefix
 		std::string response = "431 :No nickname given\r\n";
 		serverResponse.setAction(ServerResponse::SEND);
 		serverResponse.setResponse(response);
 		serverResponse.setClientsToSend(user->getUserFd());
-		std::cout << serverResponse.getResponse() << std::endl;
 		return;
 	}
 	if (!isValidNick(newNick))
@@ -71,11 +63,9 @@ NickCommand::NickCommand(ServerData& serverData, ClientMessage& clientMessage) :
 	}
 
 	user->setNickname(newNick);
-	serverResponse.setAction(ServerResponse::SEND);
-	serverResponse.setClientsToSend(user->getUserFd());
-	// TODO - also send to other user, that someone changed the nickname
 
-	// Message to client = serverResponse.data
+	// TODO - also send to other user, that someone changed the nickname? in rooms?
+
 	std::string response = ":";
 	response.append(oldNick);
 	response.append("!");
@@ -85,17 +75,19 @@ NickCommand::NickCommand(ServerData& serverData, ClientMessage& clientMessage) :
 	response.append(" NICK :");
 	response.append(newNick);
 	response.append("\r\n");
+
 	if (user->getUsername() == "" || user->getHostname() == "")
 		serverResponse.setAction(ServerResponse::NOSEND);
+	else
+		serverResponse.setAction(ServerResponse::SEND);
 	serverResponse.setResponse(response);
-	std::cout << serverResponse.getResponse() << std::endl;
+	serverResponse.setClientsToSend(user->getUserFd());
 }
 
 NickCommand::~NickCommand() {}
 
 NickCommand::NickCommand(NickCommand const& refObj) : serverData(refObj.serverData), clientMessage(refObj.clientMessage) {}
 
-// will this work when there is a reference variables?
 NickCommand& NickCommand::operator=(NickCommand const& refObj)
 {
 	if (this != &refObj)
@@ -112,23 +104,6 @@ ServerResponse NickCommand::getServerResponse()
 }
 
 // ---- PRIVATE ----- //
-// void NickCommand::parseClientMessage()
-// {
-// 	std::string delimiter = " \r\n";
-// 	int pos = 0;
-// 	std::string token;
-// 	std::string parametersString = clientMessage.getParametersString();
-
-// 	while (parametersString.find_first_of(delimiter) != std::string::npos)
-// 	{
-// 		std::cout << "IN" << std::endl;
-// 		pos = parametersString.find_first_of(delimiter);
-// 		token = parametersString.substr(0, pos);
-// 		clientMessage.addToParameters(token);
-// 		parametersString = parametersString.erase(0, pos + 1);
-// 	}
-// 	// clientMessage.addToParameters(parametersString);
-// }
 
 std::string NickCommand::getNewNickname()
 {
@@ -169,12 +144,12 @@ bool NickCommand::isValidNick(std::string& nick)
 bool NickCommand::isAlreadyUsedNick(std::string& nick)
 {
 	User* user = serverData.users.findUser(nick);
+
 	if (user == NULL)
-	{
 		user = serverData.waitingUsers.findUser(nick);
-	}
 
 	if (user == NULL)
 		return (true);
+
 	return (false);
 }
