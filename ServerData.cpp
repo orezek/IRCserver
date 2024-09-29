62148965324548860e3707076dc5abdfab4034a1
O.R <orezek@seznam.cz>
2024-09-29 10:19:47 +0200

    Implement: populate port number to have effect on the server

--- ServerData.cpp
+++ ServerData.cpp
@@ -3,16 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ServerData.cpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
+/*   By: orezek <orezek@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/22 20:01:51 by orezek            #+#    #+#             */
-/*   Updated: 2024/09/27 16:42:07 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/09/29 10:17:03 by orezek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "ServerData.hpp"
-
 
 ServerData::ServerData() : rooms(), waitingUsers(), users() {};
 
@@ -20,6 +18,7 @@ ServerData::~ServerData() {};
 
 std::string ServerData::SERVER_PASSWORD = "";
 std::string ServerData::SERVER_NAME = "irc.brdevstudio.com";
+int ServerData::SERVER_PORT_NUMBER = -1;
 
 void ServerData::setServerPassword(const std::string &serverPassword)
 {
@@ -34,4 +33,15 @@ const std::string &ServerData::getServerPassword(void)
 const std::string &ServerData::getServerName(void)
 {
 	return (ServerData::SERVER_NAME);
+}
+
+const int &ServerData::getServerPortNumber(void)
+{
+	return (ServerData::SERVER_PORT_NUMBER);
+}
+
+
+void ServerData::setServerPortNumber(const int &serverPortNumber)
+{
+	ServerData::SERVER_PORT_NUMBER = serverPortNumber;
 }
--- ServerData.hpp
+++ ServerData.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ServerData.hpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
+/*   By: orezek <orezek@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/22 19:57:16 by orezek            #+#    #+#             */
-/*   Updated: 2024/09/27 16:41:29 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/09/29 10:16:21 by orezek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,19 @@ class ServerData
 	public:
 		ServerData();
 		~ServerData();
-		std::vector<int> fileDsDb; // will be deleted!
+		std::vector<int> fileDsDb;  // will be deleted!
 		Rooms rooms;
 		Users waitingUsers;
 		Users users;
 		static void setServerPassword(const std::string &serverPassword);
 		static const std::string &getServerPassword(void);
 		static const std::string &getServerName(void);
+		static const int &getServerPortNumber(void);
+		static void setServerPortNumber(const int &setServerPortNumber);
 
 	private:
 		static std::string SERVER_PASSWORD;
 		static std::string SERVER_NAME;
-
+		static int SERVER_PORT_NUMBER;
 };
+
