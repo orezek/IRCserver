d17751a5f364e1e7666993b6b525f503ee4fb71f
m-bartos <m.bartos96@seznam.cz>
2024-09-30 13:52:59 +0200

    Fix: User.hpp

--- include/User.hpp
+++ include/User.hpp
@@ -39,7 +39,7 @@ class User
 		void setServername(std::string servername);
 		void setValidPass(bool passValue);
 		void setValidNick(bool nickValue);
-		void setValidUser(bool userValue)
+		void setValidUser(bool userValue);
 
 	private:
 		int userFd;
