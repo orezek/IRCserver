1a1fc40f4ee14db3200ca69f381aac54b41ff805
m-bartos <28412526+m-bartos@users.noreply.github.com>
2024-10-05 12:48:03 +0200

    Update: ServerData.hpp

--- include/ServerData.hpp
+++ include/ServerData.hpp
@@ -6,35 +6,40 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/22 19:57:16 by orezek            #+#    #+#             */
-/*   Updated: 2024/10/05 12:04:45 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/10/05 12:39:13 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+#include <map>
 #include <vector>
 
 #include "ClientRequestQueue.hpp"
 #include "Rooms.hpp"
 #include "ServerResponseQueue.hpp"
 #include "Users.hpp"
-
 class ServerData
 {
 	public:
 		ServerData();
 		~ServerData();
+
+		std::map<int, Client> clients;
 		Rooms rooms;
-		Users waitingUsers;                                                // ?will be changed to a map of waiting clients <fd, client>?
-		Users users;                                                       // will be changed to map of clients <fd, client>
-		ClientRequestQueue splittedClientRequests;                         // will be deleted
-		ServerResponseQueue serverResponses;                               // will be deleted
-		std::vector<int> fileDsDb;                                         // will be deleted
+
+		// DO NOT USE ANYMORE:
+		Users waitingUsers;                         // will be deleted
+		Users users;                                // will be deleted
+		ClientRequestQueue splittedClientRequests;  // will be deleted
+		ServerResponseQueue serverResponses;        // will be deleted
+		std::vector<int> fileDsDb;                  // will be deleted
+		void validateWaitingUser(int clientFd);     // this functionality is moved to User class as "bool User::isValidServerUser()"
+
 		static void setServerPassword(const std::string &serverPassword);  // delete if const?
 		static const std::string &getServerPassword(void);
 		static const std::string &getServerName(void);
 		static const int &getServerPortNumber(void);
 		static void setServerPortNumber(const int &setServerPortNumber);  // delete if const?
-		void validateWaitingUser(int clientFd);                           // rename to validateWaitingClient
 
 	private:
 		static std::string SERVER_PASSWORD;  // should be const?
