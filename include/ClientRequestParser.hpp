e3cb670b31d572c7ece173b096e1af5279403f3d
m-bartos <28412526+m-bartos@users.noreply.github.com>
2024-10-07 11:52:49 +0200

    Update: parsing in ClientRequestParser

--- include/ClientRequestParser.hpp
+++ include/ClientRequestParser.hpp
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 18:09:39 by mbartos           #+#    #+#             */
-/*   Updated: 2024/10/01 20:02:32 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/10/07 10:37:58 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,7 @@ class ClientRequestParser
 		void parseCommandString();
 		void parseParameters();
 
-		void parseParametersByNewline();
+		void parseParametersBySpace();
 		void parseParametersAsOneText();
+		void parseParametersAsUser();
 };
