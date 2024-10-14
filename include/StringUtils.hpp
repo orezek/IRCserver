2a69c0b1a9241281187c3ce14da7cce801cf9bf0
m-bartos <28412526+m-bartos@users.noreply.github.com>
2024-10-14 21:12:27 +0200

    Implement: toLowerCase() in StringUtils class

--- include/StringUtils.hpp
+++ include/StringUtils.hpp
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 16:32:13 by mbartos           #+#    #+#             */
-/*   Updated: 2024/09/26 16:45:17 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/10/14 21:06:35 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,5 @@ class StringUtils
 {
 	public:
 		static std::string toUpperCase(const std::string& str);
+		static std::string toLowerCase(const std::string& str);
 };
--- src/StringUtils.cpp
+++ src/StringUtils.cpp
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 16:33:08 by mbartos           #+#    #+#             */
-/*   Updated: 2024/09/26 16:44:41 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/10/14 21:06:18 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,16 @@ std::string StringUtils::toUpperCase(const std::string& str)
 	for (size_t i = 0; i < result.size(); ++i)
 	{
 		result[i] = std::toupper(result[i]);
+	}
+	return result;
+}
+
+std::string StringUtils::toLowerCase(const std::string& str)
+{
+	std::string result = str;
+	for (size_t i = 0; i < result.size(); ++i)
+	{
+		result[i] = std::tolower(result[i]);
 	}
 	return result;
 }
