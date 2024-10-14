6e9a288c12811ed25ad5075705827831a828dd53
m-bartos <28412526+m-bartos@users.noreply.github.com>
2024-10-14 22:54:22 +0200

    Update: RoomName in Types.hpp

--- include/Types.hpp
+++ include/Types.hpp
@@ -6,11 +6,11 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 20:00:26 by mbartos           #+#    #+#             */
-/*   Updated: 2024/10/14 20:50:15 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/10/14 22:29:14 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 typedef int ClientID;
-typedef int RoomName;
+typedef std::string RoomName;
