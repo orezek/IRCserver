6f0afd80102a394d46538c0e84bc9c66e557c0e4
m-bartos <28412526+m-bartos@users.noreply.github.com>
2024-10-20 12:14:48 +0200

    Implement: Quit command as a child class of ABaseCommand

--- src/ProcessData.cpp
+++ src/ProcessData.cpp
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 22:25:17 by orezek            #+#    #+#             */
-/*   Updated: 2024/10/19 11:28:21 by mbartos          ###   ########.fr       */
+/*   Updated: 2024/10/19 12:09:50 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,6 +47,7 @@ ProcessData::ProcessData(Client *client, ClientRequest *clientRequest) : client(
 		else if (StringUtils::toUpperCase(clientMessage.getCommandString()) == "QUIT")
 		{
 			Commands::Quit quitCommand(client, clientMessage);
+			quitCommand.execute();
 		}
 		else
 		{
@@ -105,6 +106,7 @@ ProcessData::ProcessData(Client *client, ClientRequest *clientRequest) : client(
 		else if (StringUtils::toUpperCase(clientMessage.getCommandString()) == "QUIT")
 		{
 			Commands::Quit quitCommand(client, clientMessage);
+			quitCommand.execute();
 		}
 		else
 		{
