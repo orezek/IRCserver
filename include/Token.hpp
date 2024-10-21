bcafe8d0e78f86f1e579c44cd3128ed382b3b561
m-bartos <28412526+m-bartos@users.noreply.github.com>
2024-10-21 21:05:15 +0200

    Implement: Token class

---
+++ include/Token.hpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Token.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/21 19:24:12 by mbartos           #+#    #+#             */
+/*   Updated: 2024/10/21 21:00:29 by mbartos          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <string>
+
+class Token
+{
+	public:
+		enum Type
+		{
+			NOT_ASSIGNED = -1,  // DO WE NEED IT?
+			PREFIX,
+			COMMAND,
+			NICK_NAME,        // NICK command
+			ROOM_NAME,        // JOIN command
+			ROOM_PASSWORD,    // JOIN command
+			SERVER_PASSWORD,  // PASS command
+			MESSAGE,          // QUIT command,
+			USER_NAME,        // USER command
+			HOST_NAME,        // USER command
+			SERVER_NAME,      // USER command
+			REAL_NAME,        // USER command
+			OTHER = 99
+		};
+
+		Token(Type type, std::string text);
+		// Token();
+		~Token();
+		Token(Token const&);
+		Token& operator=(Token const&);
+
+		Type getType() const;
+		std::string getText() const;
+
+		void setType(Token::Type newType);
+
+	private:
+		std::string text;
+		Token::Type type;
+};
