58ee0fd7295201b8b8247b6fb1e9f7ca870a8eef
O.R <orezek@seznam.cz>
2024-10-30 19:06:01 +0100

    Implement: join response 366,353 and other helper functions

--- include/Room.hpp
+++ include/Room.hpp
@@ -6,12 +6,13 @@
 /*   By: orezek <orezek@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/22 19:55:15 by orezek            #+#    #+#             */
-/*   Updated: 2024/10/30 03:38:39 by orezek           ###   ########.fr       */
+/*   Updated: 2024/10/30 18:18:04 by orezek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
+#include <ClientManager.hpp>
 #include <algorithm>
 #include <sstream>
 #include <string>
@@ -34,20 +35,24 @@ class Room
 		void setPassword(std::string password);
 		bool isPasswordRequired(void);
 		// Topic
-		const std::string &getTopic(void) const;
+		const std::string& getTopic(void) const;
 		void setTopic(const std::string message);
 		bool isTopicSet(void);
 		// Operator
 		bool isOperator(const int clientFd);
 		void addOperator(const int clientFd);
 		void removeOperator(const int clientFd);
+		// get read-only vector reference for iterating operations
+		const std::vector<int>& getAllClients() const;
+		const int getNoClients(void) const;
 
 	private:
 		std::string roomName;
-		std::string password; // mode -k
+		std::string password;  // mode -k
 		std::string topic;
 		std::vector<int> clientFds;
 		std::vector<int> operators;
+		static int clientCount;
 		// to do //
 };
 
